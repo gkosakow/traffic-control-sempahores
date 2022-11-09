6bf4b51eaa3fc34d715d6797e1b0cc11ee21509d
Greg Kosakowski <99287183+gkosakow@users.noreply.github.com>
2022-11-09 12:01:14 -0500

    using template from provided gist

--- traffic.cpp
+++ traffic.cpp
@@ -1,8 +1,47 @@
-#include <iostream>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <semaphore.h>
 
-using namespace std;
+sem_t semaphore;
 
-int main()
-{
-    cout << "Hello World" << endl;
+void * threadfunc(void* arg) {
+    while (1) {
+        sem_wait(&semaphore);
+        printf("Hello from da thread!\n");
+        sem_post(&semaphore);
+        sleep(1);
+    }
+    return NULL;
+}
+
+int main(void) {
+    
+    // initialize semaphore, only to be used with threads in this process, set value to 1
+    sem_init(&semaphore, 0, 1);
+    
+    pthread_t *mythread;
+    
+    mythread = (pthread_t *)malloc(sizeof(*mythread));
+    
+    // start the thread
+    printf("Starting thread, semaphore is unlocked.\n");
+    pthread_create(mythread, NULL, threadfunc, NULL);
+    
+    getchar();
+    
+    sem_wait(&semaphore);
+    printf("Semaphore locked.\n");
+    
+    // do stuff with whatever is shared between threads
+    getchar();
+    
+    printf("Semaphore unlocked.\n");
+    sem_post(&semaphore);
+    
+    getchar();
+    
+    return 0;
 }
