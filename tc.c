9a627cda5e0bf1060ad4cfe41c4a63260f2433de
Greg Kosakowski <99287183+gkosakow@users.noreply.github.com>
2022-11-15 23:09:55 -0500

    matt and greg late nightzzz

--- tc.c
+++ tc.c
@@ -5,277 +5,289 @@
 #include <unistd.h>
 #include <semaphore.h>
 
-sem_t semaphore;
-
-sem_t NSI, NSO, NSM;            // north-side in, north-side out, north-side mid
-sem_t SSI, SSO, SSM;            // south-side in, south-side out, north-side mid
-sem_t ESI, ESO, ESM;            // east-side in, east-side out, east-side mid
-sem_t WSI, WSO, WSM;            // west-side in, west-side out, west-side mid
-sem_t NWM, NEM, SEM, SWM;       // northwest mid, northeast mid, southeast mid, southwest mid
-sem_t* SEM_ARRAY[] = {&NSI, &NSO, &NSM, &SSI, &SSO, &SSM, &ESI, &ESO, &ESM, &WSI, &WSO, &WSM, &NWM, &NEM, &SEM, &SWM};
+sem_t headLock;                                             // arrival at intersection
+sem_t NE, NN, NW, WN, WW, WS, SW, SS, SE, ES, EE, EN;        // northwest mid, northeast mid, southeast mid, southwest mid
+sem_t *SEM_ARRAY[] = {&NE, &NN, &NW, &WN, &WW, &WS, &SW, &SS, &SE, &ES, &EE, &EN};
 
 typedef struct _directions{
+int carID;
 char dir_original;
 char dir_target;
 } directions;
 
-// void threadfunc() {
-//     while (1) {
-//         sem_wait(&semaphore);
-//         printf("Hello from da thread!\n");
-//         sem_post(&semaphore);
-//         sleep(1);
-//     }
-// }
+void *blockIntersection(void *d){
+    directions *carPtr = (directions *)d;
+    printf("%c", carPtr->dir_original); //DEBUGGER
+    printf("%c\t", carPtr->dir_target);   //DEBUGGER
+    printf("Blocking intersection\n");    //DEBUGGER
 
-void blockIntersection(directions d){
-    printf("%c", d.dir_original);
-    printf("%c", d.dir_target);
-    if (d.dir_original == 'N'){
-        // RIGHT TURN IN NORTH DIRECTION
-        if (d.dir_target == "E"){
-            sem_wait(&SSI);
-            sem_wait(&ESO);
-            return;
-        
-        // STRAIGHT IN NORTH DIRECTION
-        } else if (d.dir_target == 'N'){
-            printf("Working correctly");
-            sem_wait(&SSI);
-            sem_wait(&SEM);
-            sem_wait(&ESM);
-            sem_wait(&NEM);
-            sem_wait(&NSO);
-            return;
-
-        // LEFT TURN IN NORTH DIRECTION
-        } else if (d.dir_target == "W"){
-            sem_wait(&SSI);
-            sem_wait(&SSM);
-            sem_wait(&WSM);
-            sem_wait(&WSO);
-            return;
+    if (carPtr->dir_original == 'N'){
+        if(carPtr->dir_target == 'N'){
+            printf("Car 1 hit correct conditions\n"); //DEBUGGER
+            sem_wait(&NN);
+            sem_wait(&WN);
+            sem_wait(&WW);
+            sem_wait(&WS);
+            sem_wait(&SE);
+            sem_wait(&EE);
+            sem_wait(&EN);
+        } else if(carPtr->dir_target == 'E'){
+            sem_wait(&NE);
+            sem_wait(&SE);
+            sem_wait(&EE);
+        } else if(carPtr->dir_target == 'W'){
+            sem_wait(&NW);
+            sem_wait(&WW);
+            sem_wait(&WS);
+            sem_wait(&SW);
+            sem_wait(&SS);
+            sem_wait(&EE);
+            sem_wait(&EN);
         }
-    } else if (d.dir_original == "E"){
-        if (d.dir_target == "S"){
-            sem_wait(&WSI);
-            sem_wait(&SSO);
-            return;
-
-        } else if (d.dir_target == "E"){
-            sem_wait(&WSI);
-            sem_wait(&SWM);
-            sem_wait(&SSM);
-            sem_wait(&SEM);
-            sem_wait(&ESO);
-            return;
-
-        } else if (d.dir_target == "N"){
-            sem_wait(&WSI);
-            sem_wait(&WSM);
-            sem_wait(&NSM);
-            sem_wait(&NSO);
-            return;
-        }
-    } else if (d.dir_original == "S"){
-        if (d.dir_target == "W"){
-            sem_wait(&NSI);
-            sem_wait(&WSO);
-            return;
-
-        } else if (d.dir_target == "S"){
-            sem_wait(&NSI);
-            sem_wait(&NWM);
-            sem_wait(&WSM);
-            sem_wait(&SWM);
-            sem_wait(&SSO);
-            return;
-
-        } else if (d.dir_target == "E"){
-            sem_wait(&NSI);
-            sem_wait(&NSM);
-            sem_wait(&ESM);
-            sem_wait(&ESO);
-            return;
-        }
-    } else if (d.dir_original == "W"){
-        if (d.dir_target == "S"){
-            sem_wait(&ESI);
-            sem_wait(&ESM);
-            sem_wait(&SSM);
-            sem_wait(&SSO);
-            return;
-
-        } else if (d.dir_target == "W"){
-            sem_wait(&ESI);
-            sem_wait(&NEM);
-            sem_wait(&NSM);
-            sem_wait(&NWM);
-            sem_wait(&WSO);
-            return;
-
-        } else if (d.dir_target == "N"){
-            sem_wait(&ESI);
-            sem_wait(&NSO);
-            return;
-
-        } 
-    } else {
-        printf("\nYou passed everything, something is broken...........");
     }
+    if (carPtr->dir_original == 'E'){
+        if(carPtr->dir_target == 'E'){
+            sem_wait(&EE);
+            sem_wait(&NE);
+            sem_wait(&NN);
+            sem_wait(&NW);
+            sem_wait(&WS);
+            sem_wait(&SS);
+            sem_wait(&SE);
+        } else if(carPtr->dir_target == 'S'){
+            sem_wait(&ES);
+            sem_wait(&WS);
+            sem_wait(&SS);
+        } else if(carPtr->dir_target == 'N'){
+            sem_wait(&EN);
+            sem_wait(&NN);
+            sem_wait(&NW);
+            sem_wait(&WN);
+            sem_wait(&WW);
+            sem_wait(&SS);
+            sem_wait(&SE);
+        }
+    }
+    if (carPtr->dir_original == 'S'){
+        if(carPtr->dir_target == 'S'){
+            sem_wait(&SS);
+            sem_wait(&ES);
+            sem_wait(&EE);
+            sem_wait(&EN);
+            sem_wait(&NW);
+            sem_wait(&WW);
+            sem_wait(&WS);
+        } else if(carPtr->dir_target == 'W'){
+            sem_wait(&SW);
+            sem_wait(&NW);
+            sem_wait(&WW);
+        } else if(carPtr->dir_target == 'E'){
+            sem_wait(&SE);
+            sem_wait(&EE);
+            sem_wait(&EN);
+            sem_wait(&NE);
+            sem_wait(&NN);
+            sem_wait(&WW);
+            sem_wait(&WS);
+        }
+    }
+    if (carPtr->dir_original == 'W'){
+        if(carPtr->dir_target == 'W'){
+            printf("Car 2 hit correct conditional\n"); //DEBUGGER
+            sem_wait(&WW);
+            sem_wait(&SW);
+            sem_wait(&SS);
+            sem_wait(&SE);
+            sem_wait(&EN);
+            sem_wait(&NN);
+            sem_wait(&NW);
+        } else if(carPtr->dir_target == 'N'){
+            sem_wait(&WN);
+            sem_wait(&EN);
+            sem_wait(&NN);
+        } else if(carPtr->dir_target == 'S'){
+            sem_wait(&WS);
+            sem_wait(&SS);
+            sem_wait(&SE);
+            sem_wait(&ES);
+            sem_wait(&EE);
+            sem_wait(&NN);
+            sem_wait(&NW);
+        }
+    }
+
+    return (void *) NULL;
 }
 
-void unblockIntersection(directions d){
-        if (d.dir_original == "N"){
-        // RIGHT TURN IN NORTH DIRECTION
-        if (d.dir_target == "E"){
-            sem_post(&SSI);
-            sem_post(&ESO);
-            return;
-        
-        // STRAIGHT IN NORTH DIRECTION
-        } else if (d.dir_target == "N"){
-            sem_post(&SSI);
-            sem_post(&SEM);
-            sem_post(&ESM);
-            sem_post(&NEM);
-            sem_post(&NSO);
-            return;
+void *unblockIntersection(void *d){
+    directions *carPtr = (directions *)d;
+    printf("%c", carPtr->dir_original); //DEBUGGER
+    printf("%c\t", carPtr->dir_target);   //DEBUGGER
+    printf("Unblocking intersection\n");    //DEBUGGER
 
-        // LEFT TURN IN NORTH DIRECTION
-        } else if (d.dir_target == "W"){
-            sem_post(&SSI);
-            sem_post(&SSM);
-            sem_post(&WSM);
-            sem_post(&WSO);
-            return;
+    if (carPtr->dir_original == 'N'){
+        if(carPtr->dir_target == 'N'){
+            sem_post(&NN);
+            sem_post(&WN);
+            sem_post(&WW);
+            sem_post(&WS);
+            sem_post(&SE);
+            sem_post(&EE);
+            sem_post(&EN);
+        } else if(carPtr->dir_target == 'E'){
+            sem_post(&NE);
+            sem_post(&SE);
+            sem_post(&EE);
+        } else if(carPtr->dir_target == 'W'){
+            sem_post(&NW);
+            sem_post(&WW);
+            sem_post(&WS);
+            sem_post(&SW);
+            sem_post(&SS);
+            sem_post(&EE);
+            sem_post(&EN);
         }
-    } else if (d.dir_original == "E"){
-        if (d.dir_target == "S"){
-            sem_post(&WSI);
-            sem_post(&SSO);
-            return;
-
-        } else if (d.dir_target == "E"){
-            sem_post(&WSI);
-            sem_post(&SWM);
-            sem_post(&SSM);
-            sem_post(&SEM);
-            sem_post(&ESO);
-            return;
-
-        } else if (d.dir_target == "N"){
-            sem_post(&WSI);
-            sem_post(&WSM);
-            sem_post(&NSM);
-            sem_post(&NSO);
-            return;
-        }
-    } else if (d.dir_original == "S"){
-        if (d.dir_target == "W"){
-            sem_post(&NSI);
-            sem_post(&WSO);
-            return;
-
-        } else if (d.dir_target == "S"){
-            sem_post(&NSI);
-            sem_post(&NWM);
-            sem_post(&WSM);
-            sem_post(&SWM);
-            sem_post(&SSO);
-            return;
-
-        } else if (d.dir_target == "E"){
-            sem_post(&NSI);
-            sem_post(&NSM);
-            sem_post(&ESM);
-            sem_post(&ESO);
-            return;
-        }
-    } else if (d.dir_original == "W"){
-        if (d.dir_target == "S"){
-            sem_post(&ESI);
-            sem_post(&ESM);
-            sem_post(&SSM);
-            sem_post(&SSO);
-            return;
-
-        } else if (d.dir_target == "W"){
-            sem_post(&ESI);
-            sem_post(&NEM);
-            sem_post(&NSM);
-            sem_post(&NWM);
-            sem_post(&WSO);
-            return;
-
-        } else if (d.dir_target == "N"){
-            sem_post(&ESI);
-            sem_post(&NSO);
-            return;
-
-        } 
-    } else {
-        printf("\nYou passed everything, something is broken...........");
     }
+    if (carPtr->dir_original == 'E'){
+        if(carPtr->dir_target == 'E'){
+            sem_post(&EE);
+            sem_post(&NE);
+            sem_post(&NN);
+            sem_post(&NW);
+            sem_post(&WS);
+            sem_post(&SS);
+            sem_post(&SE);
+        } else if(carPtr->dir_target == 'S'){
+            sem_post(&ES);
+            sem_post(&WS);
+            sem_post(&SS);
+        } else if(carPtr->dir_target == 'N'){
+            sem_post(&EN);
+            sem_post(&NN);
+            sem_post(&NW);
+            sem_post(&WN);
+            sem_post(&WW);
+            sem_post(&SS);
+            sem_post(&SE);
+        }
+    }
+    if (carPtr->dir_original == 'S'){
+        if(carPtr->dir_target == 'S'){
+            sem_post(&SS);
+            sem_post(&ES);
+            sem_post(&EE);
+            sem_post(&EN);
+            sem_post(&NW);
+            sem_post(&WW);
+            sem_post(&WS);
+        } else if(carPtr->dir_target == 'W'){
+            sem_post(&SW);
+            sem_post(&NW);
+            sem_post(&WW);
+        } else if(carPtr->dir_target == 'E'){
+            sem_post(&SE);
+            sem_post(&EE);
+            sem_post(&EN);
+            sem_post(&NE);
+            sem_post(&NN);
+            sem_post(&WW);
+            sem_post(&WS);
+        }
+    }
+    if (carPtr->dir_original == 'W'){
+        if(carPtr->dir_target == 'W'){
+            sem_post(&WW);
+            sem_post(&SW);
+            sem_post(&SS);
+            sem_post(&SE);
+            sem_post(&EN);
+            sem_post(&NN);
+            sem_post(&NW);
+        } else if(carPtr->dir_target == 'N'){
+            sem_post(&WN);
+            sem_post(&EN);
+            sem_post(&NN);
+        } else if(carPtr->dir_target == 'S'){
+            sem_post(&WS);
+            sem_post(&SS);
+            sem_post(&SE);
+            sem_post(&ES);
+            sem_post(&EE);
+            sem_post(&NN);
+            sem_post(&NW);
+        }
+    }
+    
+    return (void *) NULL;
 }
 
-void ArriveIntersection(directions d){
-    printf("arriving");
+void ArriveIntersection(void* d){
+    directions *carPtr = (directions *)d;
+    printf("%d\t", carPtr->carID);
+    printf("arriving\n");
     sleep(2);
-    // wait head_of_line lock
-    // Lock dir_target;
-    // lock  dir_original;
+    sem_wait(&headLock);
     blockIntersection(d);
 }
 
-void CrossIntersection(directions d){
-    //Unlock head lock
-    if (d.dir_original == "N"){
-        sem_post(&SSI);
-    } else if (d.dir_original == "E"){
-        sem_post(&WSI);
-    } else if (d.dir_original == "S"){
-        sem_post(&NSI);
-    } else if (d.dir_original = "W"){
-        sem_post(&ESI);
-    } else {
-        printf("This is fucked");
-    }
-    printf("crossing");
+void CrossIntersection(void* d){
+    directions *carPtr = (directions *)d;
+    printf("%d\t", carPtr->carID);
+    printf("crossing\n");
+    sem_post(&headLock);
     sleep(4); // x= left, right, straight
-
 }
 
-void ExitIntersection(directions d){
-    printf("exiting");
-    // Unlock dir_target
+void ExitIntersection(void* d){
+    directions *carPtr = (directions *)d;
+    printf("%d\t", carPtr->carID);
+    printf("exiting\n");
     unblockIntersection(d);
 }
 
-void * Car(directions dir) {
-    ArriveIntersection(dir);
-    CrossIntersection(dir);
-    ExitIntersection(dir);
+void * Car(void* d) {
+    directions *carPtr = (directions *)d;
+    ArriveIntersection(d);
+    CrossIntersection(d);
+    ExitIntersection(d);
+    printf("Car %d\t", carPtr->carID);
+    printf("finished executing\n");
+    return NULL;
 }
 
 int main(void) {
     
+    sem_init(&headLock, 0, 1);
+    
     // initialize semaphore, only to be used with threads in this process, set value to 1
-    for(int i = 0; i < 16; i++){
+    for(int i = 0; i < 12; i++){
         sem_init(SEM_ARRAY[i], 0, 1);
     }
 
-    directions p1;
-    p1.dir_original = 'N';
-    p1.dir_target = 'N';
+    pthread_t thread1;
+    pthread_t thread2;
+    directions *car1Ptr;
+    car1Ptr = (directions *)malloc(sizeof(directions));
+    car1Ptr->carID = 1;
+    car1Ptr->dir_original = 'N';
+    car1Ptr->dir_target = 'N';
+    printf("%c", car1Ptr->dir_original);
+    printf("%c\n", car1Ptr->dir_target);
 
-    pthread_t *mythread;
-    
-    mythread = (pthread_t *)malloc(sizeof(*mythread));
-    
-    pthread_create(mythread, NULL, (void*)Car, &p1);
-    
-    getchar();
-    
+    directions *car2Ptr;
+    car2Ptr = (directions *)malloc(sizeof(directions));
+    car2Ptr->carID = 2;
+    car2Ptr->dir_original = 'W';
+    car2Ptr->dir_target = 'W';
+    printf("%c", car2Ptr->dir_original);
+    printf("%c\n", car2Ptr->dir_target);
+
+    pthread_create(&thread1, NULL, Car, car1Ptr);
+    pthread_create(&thread2, NULL, Car, car2Ptr);
+
+    // printf("after pthread_create\n");
+    pthread_join(thread1, NULL);
+    pthread_join(thread2, NULL);
+    printf("\n");
     return 0;
 }
