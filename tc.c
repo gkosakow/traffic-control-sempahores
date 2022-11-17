3ee653117bfb650e5b3f565b6bed1050f1eacf74
MatsoA <19639672+MatsoA@users.noreply.github.com>
2022-11-16 21:46:11 -0500

    fully done counters

--- tc.c
+++ tc.c
@@ -319,8 +319,12 @@ void *unblockPath(void *d)
         }
         else if (carPtr->dir_target == 'E')
         {
-            sem_post(&SE);
-            sem_post(&EE);
+            num_SE--;
+            if (num_SE == 0)
+                sem_post(&SE);
+            num_EE--;
+            if (num_EE == 0)
+                sem_post(&EE);
         }
         else if (carPtr->dir_target == 'W')
         {
@@ -353,26 +357,65 @@ void *unblockPath(void *d)
     {
         if (carPtr->dir_target == 'E')
         {
-            sem_post(&NE);
-            sem_post(&NN);
-            sem_post(&NW);
-            sem_post(&WS);
-            sem_post(&SS);
-            sem_post(&SE);
+            num_NE--;
+            if (num_NE == 0)
+                sem_post(&NE);
+
+            num_NN--;
+            if (num_NN == 0)
+                sem_post(&NN);
+
+            num_NW--;
+            if (num_NW == 0)
+                sem_post(&NW);
+
+            num_WS--;
+            if (num_WS == 0)
+                sem_post(&WS);
+
+            num_SS--;
+            if (num_SS == 0)
+                sem_post(&SS);
+
+            num_SE--;
+            if (num_SE == 0)
+                sem_post(&SE);
         }
         else if (carPtr->dir_target == 'S')
         {
-            sem_post(&WS);
-            sem_post(&SS);
+            num_WS--;
+            if (num_WS == 0)
+                sem_post(&WS);
+
+            num_SS--;
+            if (num_SS == 0)
+                sem_post(&SS);
         }
         else if (carPtr->dir_target == 'N')
         {
-            sem_post(&NN);
-            sem_post(&NW);
-            sem_post(&WN);
-            sem_post(&WW);
-            sem_post(&SS);
-            sem_post(&SE);
+            num_NN--;
+            if (num_NN == 0)
+                sem_post(&WS);
+
+            num_NW--;
+            if (num_NW == 0)
+                sem_post(&NW);
+
+            num_WN--;
+            if (num_WN == 0)
+                sem_post(&WN);
+
+            num_WW--;
+            if (num_WW == 0)
+                sem_post(&WW);
+
+            num_SS--;
+            if (num_SS == 0)
+                sem_post(&SS);
+
+            num_SE--;
+            if (num_SE == 0)
+                sem_post(&SE);
         }
     }
     if (carPtr->dir_original == 'S')
@@ -444,26 +487,66 @@ void *unblockPath(void *d)
     {
         if (carPtr->dir_target == 'W')
         {
-            sem_post(&SW);
-            sem_post(&SS);
-            sem_post(&SE);
-            sem_post(&EN);
-            sem_post(&NN);
-            sem_post(&NW);
+            num_SW--;
+            if (num_SW == 0)
+                sem_post(&SW);
+
+            num_SS--;
+            if (num_SS == 0)
+                sem_post(&SS);
+
+            num_SE--;
+            if (num_SE == 0)
+                sem_post(&SE);
+
+            num_EN--;
+            if (num_EN == 0)
+                sem_post(&EN);
+
+            num_NN--;
+            if (num_NN == 0)
+                sem_post(&NN);
+
+            num_NW--;
+            if (num_NW == 0)
+                sem_post(&NW);
         }
         else if (carPtr->dir_target == 'N')
         {
-            sem_post(&EN);
-            sem_post(&NN);
+            num_EN--;
+            if (num_EN == 0)
+                sem_post(&EN);
+
+            num_EN--;
+            if (num_EN == 0)
+                sem_post(&EN);
         }
         else if (carPtr->dir_target == 'S')
         {
-            sem_post(&SS);
-            sem_post(&SE);
-            sem_post(&ES);
-            sem_post(&EE);
-            sem_post(&NN);
+            num_SS--;
+            if (num_SS == 0)
+                sem_post(&WS);
+
+            num_SE--;
+            if (num_SE == 0)
+                sem_post(&SE);
+
+            num_ES--;
+            if (num_ES == 0)
+                sem_post(&ES);
+
+            num_EE--;
+            if (num_EE == 0)
+                sem_post(&EE);
+
+            num_NN--;
+            if (num_NN == 0)
+                sem_post(&NN);
+
             sem_post(&NW);
+            num_NW--;
+            if (num_NW == 0)
+                sem_post(&NW);
         }
     }
 
@@ -614,37 +697,95 @@ int main(void)
     pthread_t thread1;
     pthread_t thread2;
     pthread_t thread3;
+    pthread_t thread4;
+    pthread_t thread5;
+    pthread_t thread6;
+    pthread_t thread7;
+    pthread_t thread8;
 
     directions *car1Ptr;
     car1Ptr = (directions *)malloc(sizeof(directions));
-    car1Ptr->carID = 1;
+    car1Ptr->carID = 0;
+    car1Ptr->arrTime = 1.1;
     car1Ptr->dir_original = 'N';
     car1Ptr->dir_target = 'N';
 
     directions *car2Ptr;
     car2Ptr = (directions *)malloc(sizeof(directions));
-    car2Ptr->carID = 2;
-    car2Ptr->dir_original = 'S';
-    car2Ptr->dir_target = 'S';
+    car2Ptr->carID = 1;
+    car2Ptr->arrTime = 2.0;
+    car2Ptr->dir_original = 'N';
+    car2Ptr->dir_target = 'N';
 
     directions *car3Ptr;
     car3Ptr = (directions *)malloc(sizeof(directions));
-    car3Ptr->carID = 3;
-    car3Ptr->dir_original = 'W';
+    car3Ptr->carID = 2;
+    car3Ptr->arrTime = 3.3;
+    car3Ptr->dir_original = 'N';
     car3Ptr->dir_target = 'W';
 
-    start = time(NULL); // starting the timer. (NULL is initial time in int seconds)
+    directions *car4Ptr;
+    car4Ptr = (directions *)malloc(sizeof(directions));
+    car4Ptr->carID = 3;
+    car4Ptr->arrTime = 3.5;
+    car4Ptr->dir_original = 'S';
+    car4Ptr->dir_target = 'S';
 
+    directions *car5Ptr;
+    car5Ptr = (directions *)malloc(sizeof(directions));
+    car5Ptr->carID = 4;
+    car5Ptr->arrTime = 4.2;
+    car5Ptr->dir_original = 'S';
+    car5Ptr->dir_target = 'E';
+
+    directions *car6Ptr;
+    car6Ptr = (directions *)malloc(sizeof(directions));
+    car6Ptr->carID = 5;
+    car6Ptr->arrTime = 4.4;
+    car6Ptr->dir_original = 'N';
+    car6Ptr->dir_target = 'N';
+
+    directions *car7Ptr;
+    car7Ptr = (directions *)malloc(sizeof(directions));
+    car7Ptr->carID = 6;
+    car7Ptr->arrTime = 5.7;
+    car7Ptr->dir_original = 'E';
+    car7Ptr->dir_target = 'N';
+
+    directions *car8Ptr;
+    car8Ptr = (directions *)malloc(sizeof(directions));
+    car8Ptr->carID = 7;
+    car8Ptr->arrTime = 5.9;
+    car8Ptr->dir_original = 'W';
+    car8Ptr->dir_target = 'N';
+
+    start = time(NULL); // starting the timer. (NULL is initial time in int seconds)
+    spin(1);
     pthread_create(&thread1, NULL, Car, car1Ptr);
     spin(1);
     pthread_create(&thread2, NULL, Car, car2Ptr);
     spin(1);
     pthread_create(&thread3, NULL, Car, car3Ptr);
+    spin(1);
+    pthread_create(&thread4, NULL, Car, car4Ptr);
+    spin(1);
+    pthread_create(&thread5, NULL, Car, car5Ptr);
+    spin(1);
+    pthread_create(&thread6, NULL, Car, car6Ptr);
+    spin(1);
+    pthread_create(&thread7, NULL, Car, car7Ptr);
+    spin(1);
+    pthread_create(&thread8, NULL, Car, car8Ptr);
 
     // printf("after pthread_create\n");
     pthread_join(thread1, NULL);
     pthread_join(thread2, NULL);
     pthread_join(thread3, NULL);
+    pthread_join(thread4, NULL);
+    pthread_join(thread5, NULL);
+    pthread_join(thread6, NULL);
+    pthread_join(thread7, NULL);
+    pthread_join(thread8, NULL);
 
     printf("Done with main thread.\n");
     return 0;
